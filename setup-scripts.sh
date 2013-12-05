94168f7e5c7cff917141e6c68f16976bd5c57166
Faisal Memon <faisalm@ubuntu12-0.(none)>
2013-12-05 23:14:00 +0000

    ensure that latest npm is installed

--- setup-scripts.sh
+++ setup-scripts.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-echo Setup scripts after the basic setup has been done
+echo Setup scripts to run after the basic setup has been done
+
+echo Get the latest version of node via the node package manager npm
 
 sudo apt-get install npm
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n stable
